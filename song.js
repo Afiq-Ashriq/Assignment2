4950da8a670ea1b02e8cac67939f685ea8cc694a
Afiq-Ashriq <s10221040@connect.np.edu.sg>
2023-01-12 19:51:44 +0800

    Testing audio play and pausing

---
+++ song.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+    //what kind of interface we want at the start 
+    const APIKEY = "63bf64bd969f06502871adbe";
+    getsongs();
+
+
+    //[STEP] 6
+    //let's create a function to allow you to retrieve all the information in your students
+    //by default we only retrieve 10 results
+    function getsongs(limit = 10, all = true) {
+  
+      //[STEP 7]: Create our AJAX settings
+      let settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://assignment2-6397.restdb.io/rest/songs",
+        "method": "GET", //[cher] we will use GET to retrieve info
+        "headers": {
+          "content-type": "application/json",
+          "x-apikey": APIKEY,
+          "cache-control": "no-cache"
+        },
+      }
+  
+      //[STEP 8]: Make our AJAX calls
+      //Once we get the response, we modify our table content by creating the content internally. We run a loop to continously add on data
+      //RESTDb/NoSql always adds in a unique id for each data, we tap on it to have our data and place it into our links 
+      $.ajax(settings).done(function(response) {
+  
+        let content = "";
+  
+          content = `${content}<tr id='${response[0]._id}'>
+          <td>${response[0].songid}</td>
+          <td>${response[0].songurl}</td>
+          <td>${response[0].songname}</td>`
+  
+        
+  
+        //[STEP 9]: Update our HTML content
+        //let's dump the content into our table body
+        // $("#student-list tbody").html(content);
+        $("#songs").html(content);
+  
+        // $("#total-students").html(response.length);
+      });
+  
+  
+    }
+  
+    //[STEP 10]: Create our update listener
+    //here we tap onto our previous table when we click on update
+    //this is a delegation feature of jquery
+    //because our content is dynamic in nature, we listen in on the main container which is "#student-list". For each row we have a class .update to help 
+  
+
+    
+  
+  })
+  
+  const title = Math.floor(Math.random() * 2);
+  const songs = ['caramelldansen','toothache']
+  const audio = document.querySelector('#audio');
+
+  let songindex = Math.floor(Math.random() * 2);
+
+  loadSong(songs[songindex])
+
+  function loadSong(song) {
+    
+    audio.src = `music/${song}.mp3`
+
+  };
+
+
+  function playSong() {
+    audio.play();
+
+    setTimeout(function(){
+        audio.pause();
+        alert("Audio stopped");
+    },
+    10000);
+  }
+
+  
