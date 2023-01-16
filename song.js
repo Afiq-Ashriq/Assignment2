f47938bfb554fd1d32324810ec687363c4cbd733
Afiq-Ashriq <s10221040@connect.np.edu.sg>
2023-01-16 17:13:43 +0800

    Added more songs to genre 1, bug fixes used algorithm to sort an array of options to be printed out created usedSong array to fix same song playing

--- song.js
+++ song.js
@@ -1,12 +1,13 @@
 
   $(".song-button").hide();
   $("#endscreen").hide();
+  let usedSongs = []
   var points = 0
   var coins = 0
   const rounds = 5;
-  var songindex = Math.floor(Math.random() * 4);
-  const title = Math.floor(Math.random() * 4);
-  const songs = ['caramelldansen','toothache','getlucky','nooneknows']
+  var songindex = Math.floor(Math.random() * 8);
+  const title = Math.floor(Math.random() * 8);
+  const songs = ['caramelldansen','toothache','getlucky','nooneknows','africa','billiejean','livinonaprayer','takeonme']
   const audio = document.querySelector('#audio');
   let correctAns = songs[songindex];
   var timeleft = 5;
@@ -23,14 +24,13 @@
             $(this).addClass("correct");
             if(round < rounds){
               points += 150*timetaken;
-              coins = points * 0.10;
+              coins = points * 0.01;
               console.log(timetaken.toFixed(2));
               clearTimeout(test);
-              var songindex = setTimeout(newRound, 2000);
+              setTimeout(newRound, 2000);
             } else {
               setTimeout(resetGame,3000);
               $("#startbtn").hide()
-              setTimeout($("#endscreen").text(points),3000);
               setTimeout($("#endscreen").text(coins.toFixed()),3000);
               $("#endscreen").show();
             }
@@ -39,7 +39,13 @@
           else {
             clearTimeout(test);
             $(this).addClass("wrong");
-            $("#btn4").addClass("correct");
+            let options = document.getElementsByClassName("option");
+            for (var i=0;i<options.length;i++){
+              if (options[i].textContent === correctAns) {
+                options[i].classList.add("correct");
+                break;
+              }
+            }
             setTimeout(newRound, 2000);
           }
           $(".option").not(this).prop("disabled",true);
@@ -53,23 +59,35 @@
     )
 
 
+
+
     function startGame(songindex) {
+      usedSongs.push(songs[songindex]);
       loadSong(songs[songindex]);
       audio.play();
       let timeoutId = setTimeout(function(){
           audio.pause();
       },
-      10000);
-      let opt1 = Math.floor(Math.random() * 4);
-      $("#btn1").text(songs[opt1]);
-      let opt2 = Math.floor(Math.random() * 4);
-      $("#btn2").text(songs[opt2]);
-      let opt3 = Math.floor(Math.random() * 4);
-      $("#btn3").text(songs[opt3]);
-      let opt4 = Math.floor(Math.random() * 4);
-      $("#btn4").text(songs[opt4]);
+      30000);
+      //use algorithm to shuffle array and print out options with the correctAns included
+      let options = [songs[songindex]]; // create array called options with correct answer inside and push possible options so it can be printed to html
+      while (options.length < 4) {
+        let randomIndex = Math.floor(Math.random() * 8);
+        if (!options.includes(songs[randomIndex])) {
+          options.push(songs[randomIndex]);
+        }
+      }
+  
+      for (let i = options.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [options[i], options[j]] = [options[j], options[i]];
+      }
+  
+      $("#btn1").text(options[0]);
+      $("#btn2").text(options[1]);
+      $("#btn3").text(options[2]);
+      $("#btn4").text(options[3]);
       
-      return timeoutId;
     }
 
     function loadSong(song) {
@@ -81,14 +99,21 @@
 
     let round = 1;
     function newRound() {
-    let songindex = Math.floor(Math.random() * 4);
+    // To get new and different song check if it is inside usedSongs array
+    while(usedSongs.includes(songs[songindex])){
+        songindex = Math.floor(Math.random() * 8);
+    }
+    // Change global variable correctAns
+    correctAns = songs[songindex];
     round++;
     resetGame();
     startGame(songindex);
+    //To show options
     $(".song-button").show();
     return songindex;
   };
   
+  //To reset to default
   function resetGame() {
     audio.src = ""
     $(".song-button").hide();
