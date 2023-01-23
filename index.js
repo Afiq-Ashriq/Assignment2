50607543bf2e6be47fbf62c747dd8a06ea4804c6
Afiq-Ashriq <s10221040@connect.np.edu.sg>
2023-01-23 17:09:54 +0800

    Removed spotify testing pages

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-const spotifyApi = new SpotifyWebApi();
 
-const headers = {
-  Authorization: `Bearer ${"cf96a3e1604a42669a2b51dce1e161a5"}`
-};
-
-spotifyApi.searchTracks('Narkoman', {headers}).then(
-  function(data) {
-    const tracks = data.tracks.items;
-    console.log(tracks);
-  },
-  function(err) {
-    console.error(err);
-  }
-);
 
 
 
