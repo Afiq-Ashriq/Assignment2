db9153131b26b1a613143798ddd02a873b16b337
GohJiaDong <s10243744@connect.np.edu.sg>
2023-01-26 21:37:22 +0800

    updated login.js

--- js/login.js
+++ js/login.js
@@ -1,47 +1,51 @@
 /* Test Account: test@icle.com test12345 */
 
-$(document).ready(function() {
-    const apikey = "63b70aaf969f06502871aa9e";
-    let login1 = false;
-    let accountemail = null;
-    let accountpw = null;
+const apikey = "63b70aaf969f06502871aa9e";
+let login1 = false;
+let accountemail = null;
+let accountpw = null;
 
-    $("#signin-submit").on("click",function(e){
-        e.preventDefault();
-        let email = $("#email-login").val();
-        let password = $("#pw-login").val();
+$("#signinsubmit").on("click",function(e){
+    e.preventDefault();
+    let email = $("#email-login").val();
+    let password = $("#pw-login").val();
 
-        let settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://interactivedev-ab73.restdb.io/rest/account",
-            "method": "GET",
-            "headers": {
-                "content-type": "application/json",
-                "x-apikey": apikey,
-                "cache-control": "no-cache"
-            },
+    checkInfo();
+});
+checkInfo() {
+    let settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://interactivedev-ab73.restdb.io/rest/account",
+        "method": "GET",
+        "headers": {
+            "content-type": "application/json",
+            "x-apikey": apikey,
+            "cache-control": "no-cache"
+        },
+    }
+
+    $.ajax(settings).done(function (response) {
+        for (var i = 0; i < response.length && i < limit; i++) {
+            if (response[i].email == email && response[i].password == password)
+            {
+                login1 = true;
+            }
         }
-  
-        $.ajax(settings).done(function (response) {
-            for (var i = 0; i < response.length && i < limit; i++) {
-                if (response[i].email == email && response[i].password == password)
-                {
-                    login1 = true;
-                }
-            }
-            if (login1)
-            {
-                accountemail = email;
-                accountpw = password;
-                console.log("Log-in successful");
-            }
-            else 
-            {
-                console.log("Log-in unsuccessful");
-            }
-        });
+        if (login1)
+        {
+            accountemail = email;
+            accountpw = password;
+            console.log("Log-in successful");
+        }
+        else 
+        {
+            console.log("Log-in unsuccessful");
+        }
     });
+}
+
+
     // var settings = {
     // "async": true,
     // "crossDomain": true,
@@ -57,4 +61,3 @@ $(document).ready(function() {
     // $.ajax(settings).done(function (response) {
     //     console.log(response);
     // });
-})
