53c06cbfdb6e18138c32a0895b477b86fe17a39c
GohJiaDong <s10243744@connect.np.edu.sg>
2023-02-06 19:49:58 +0800

    styding gamehome

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ function getCookie(){
 var difficulty = null;
 
 $(".diffbtn").click(function() {
+    $(".diffbtn").css("color", "whitesmoke");
+    $(this).css("color", "red");
+
     difficulty = $(this).val();
     $(".diffbtn").not(this).addClass('highlight');
     $("#diffmsg").text(`You have selected ${difficulty}`);
     $("#diffmsg").fadeIn().delay(2000).fadeOut();
     console.log(difficulty);
     sessionStorage.setItem("difficulty",difficulty);
+})
+$(".genrebtn").click(function() {
+    $(".genrebtn").css("color", "whitesmoke");
+    $(this).css("color", "red");
 })
 
 
