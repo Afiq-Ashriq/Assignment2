1066169ca82d1ec79447262b343071c8d793c63d
GohJiaDong <s10243744@connect.np.edu.sg>
2023-02-06 22:57:22 +0800

    styled morr

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ var difficulty = null;
 
 $(".diffbtn").click(function() {
     $(".diffbtn").css("color", "whitesmoke");
-    $(this).css("color", "red");
+    $(this).css("color", "rgb(255, 74, 19)");
 
     difficulty = $(this).val();
     $(".diffbtn").not(this).addClass('highlight');
-    $("#diffmsg").text(`You have selected ${difficulty}`);
-    $("#diffmsg").fadeIn().delay(2000).fadeOut();
+    $("#diffmsg p").text(`${difficulty} is selected`);
+    $("#diffmsg p").fadeIn().delay(2000).fadeOut();
     console.log(difficulty);
     sessionStorage.setItem("difficulty",difficulty);
 })
