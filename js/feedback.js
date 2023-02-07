c1dd9245844f14e5f36b4554b98692b6737bbd46
Afiq-Ashriq <s10221040@connect.np.edu.sg>
2023-02-07 14:32:39 +0800

    Bug fixes,Content for about

--- js/feedback.js
+++ js/feedback.js
@@ -2,12 +2,11 @@ $(".alert").hide();
 
 $("#fbsubmit").on("click", function(e){
     e.preventDefault();
-    $("#fbname").val() = "";
-    $("#fbemail").val() = "";
-    $("#fbtextarea").val() = "";
+    $("#fbname").val("");
+    $("#fbemail").val(""); 
+    $("#fbtextarea").val("");
 
     console.log("form submitted");
-
-    // $(".alert").show();
-    // $(".alert").fadeIn().delay(3000).fadeOut();
+    $(".alert").show();
+    $(".alert").fadeIn().delay(3000).fadeOut();
 })
